2785bb7e47791b09c9a6d45a64cb2444c1b5115c
Abe <abhi.rawat456@gmail.com>
2020-03-31 11:02:49 +0530

    Requesting data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
 
 export function App() {
+  const client = new ApolloClient({
+    uri: "https://graphql-pokemon.now.sh"
+  });
   return <p>I am a pokemon</p>;
 }
