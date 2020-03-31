61201aaafe457a6012a1a7283e026dc813b25555
Abe <abhi.rawat456@gmail.com>
2020-03-31 11:13:15 +0530

    Updated structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { PokemonContainer } from "./container/PokemonContainer";
 
 export function App() {
   const client = new ApolloClient({
